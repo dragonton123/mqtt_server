80aeeb4b3674b2ffde629062db1e1c1cf6d83b85
Tossaphorn <tossaphorn.m@gmail.com>
2018-09-05 17:09:31 +0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var mosca = require('mosca');
-var device_connection = require('./controller/device_status_controller')
-var settings = {
-		port:1884
-		}
+// var device_connection = require('./controller/device_status_controller')
+var settings = { port:1883 }
 
 var server = new mosca.Server(settings);
 
@@ -12,19 +10,38 @@ console.log("ready");
 
 server.on('clientConnected', function(client) {
 	console.log('client connected', client.id);
-	let connect_index =  client.id.indexOf("mqttjs");
-	if(connect_index<0){
-		console.log("not_server")
-		device_connection.device_connected(client.id)
-	}
+	var message = {
+		topic: '/connection',
+		payload: JSON.stringify({client_id:client.id,device_connection:"connected"}), // or a Buffer
+		qos: 0, 
+		retain: false 
+	  };
+	  
+	  server.publish(message, function() {
+		console.log('done!');
+	  });
+
 
 });
 server.on('clientDisconnected', function(client) {
 	console.log('client disconnected', client.id);
-	let disconnect_index =  client.id.indexOf("mqttjs");
-	if(disconnect_index<0){
-		console.log("not_server")
-		device_connection.device_disconnected(client.id)
-	}
+	var message = {
+		topic: '/connection',
+		payload: JSON.stringify({client_id:client.id,device_connection:"disconnected"}), // or a Buffer
+		qos: 0, 
+		retain: false
+	  };
+	  
+	  server.publish(message, function() {
+		console.log('done!');
+	  });
 });
+
+
+
+server.on('ready', setup);
+
+function setup() {
+  console.log('Mosca server is up and running');
+}
 
